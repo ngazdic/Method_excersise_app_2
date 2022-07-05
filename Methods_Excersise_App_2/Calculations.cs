66602d04863f34e92abbfe32f90810847e8594b7
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-05 11:47:35 -0400

    last commit made on program, fixed classes, put everything on another window from program.cs

---
+++ Methods_Excersise_App_2/Calculations.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Methods_Excersise_App_2
+{
+    public class Calculations
+    {
+       
+
+            public static int Add(int num1, int num2)
+            {
+
+                return num1 + num2;
+
+
+            }
+
+            public static int Subtract(int num1, int num2)
+            {
+
+                return num1 - num2;
+
+
+            }
+
+            public static int Multiply(int num1, int num2)
+            {
+
+                return num1 * num2;
+
+
+            }
+
+            public static int Divide(int num1, int num2)
+            {
+
+                return num1 / num2;
+
+
+            }
+
+
+
+
+        public static int AddParams(params int[] numbers)
+        {
+            int total = 0;
+
+
+            foreach (int i in numbers)
+            {
+                total += i;
+            }
+            return total;
+        }
+        }
+    }
+
+
